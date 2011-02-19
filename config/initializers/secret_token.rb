9daab3867c98b614d0a8d525b84b25a270e6352e
Brandon Tilley <binarymuse@binarymuse.net>
2011-02-19 15:10:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthSandbox::Application.config.secret_token = '31f1df0b15ad1dc900e8a12ff574963f9edbc3cba9a79cc42a311bd3bb362d984596af6de5060638baa28a72bb1af743d49202a9eeb035332bc2783517debf6b'
